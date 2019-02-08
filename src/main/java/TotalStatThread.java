ba697f60df58d209a05122628ddf107ab159f8b2
Sotnichenko Sergey <s.sotnichenko@tinkoff.ru>
2019-02-08 19:06:21 +0300

    Init

--- src/main/java/TotalStatThread.java
+++ src/main/java/TotalStatThread.java
@@ -8,4 +8,24 @@ public class TotalStatThread extends Thread {
         m_logicTheads = logicThreads;
     }
 
+    public void run() {
+        long beforeMillis = System.currentTimeMillis();
+
+//        while (t)
+//
+//        long n = 0;
+//        long currMillis = beforeMillis;
+//        while (currMillis < beforeMillis + m_intervalMillis) {
+//            n = n + execute(client, namespace);
+//            currMillis = System.currentTimeMillis();
+//        }
+//
+//        m_current_rows_per_sec = Math.round(n/((currMillis-beforeMillis)/1000));
+//        m_total_secs += currMillis-beforeMillis;
+//        m_total_rows += n;
+//
+//        System.out.println("[" + getName() + "][" + formatDate(currMillis) + "]: " + m_current_rows_per_sec + " per sec" );
+
+    }
+
 }
